3b57b767ee3136c9a775b66ffd9b94c15ee0bc28
zzq <2428698039@qq.com>
2021-11-24 20:32:20 +0800

    Update: main.py 完工

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import ddddocr
+import json
+
+ocr = ddddocr.DdddOcr()
 
 app = Flask(__name__)
 
-@app.route('/recognition')
-def recognition():
-    return 'HelloWorld'
+
+@app.route('/recognition', methods=['POST'])
+def recognition() -> str:
+    try:
+        return json.dumps({
+            'code': 0,
+            'result': ocr.classification(img_base64=request.get_data(as_text=True))
+        })
+    except Exception as e:
+        return json.dumps({
+            'code': 1,
+            'result': str(e)
+        })
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1',
+            port=5000,
+            debug=True)
